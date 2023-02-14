8fe7eb11d3511318494738bd3da6003486229e22
ambition-kwon <prince5390@gmail.com>
2023-02-14 15:20:24 +0900

    image path수정

--- js/app.js
+++ js/app.js
@@ -2,10 +2,10 @@ const container = document.querySelector(".container");
 const left = document.querySelector("#left");
 const right = document.querySelector("#right");
 const imgarray = [
-    "../img/pic-1.jpg",
-    "../img/pic-2.jpg",
-    "../img/pic-3.jpg",
-    "../img/pic-4.jpg"];
+    "./img/pic-1.jpg",
+    "./img/pic-2.jpg",
+    "./img/pic-3.jpg",
+    "./img/pic-4.jpg"];
 let index = 0;
 function leftSlide() {
     index = index-1 === -1 ? 3 : --index;
